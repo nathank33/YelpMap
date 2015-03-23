5e5e1cc6603a34ae8093716813734db7a2e61071
nathank33 <nathanbraunguitar@gmail.com>
2015-03-23 14:01:00 -0700

    YelpAPI importted

--- app/src/main/java/com/nathanbraun/yelpmap/MainActivity.java
+++ app/src/main/java/com/nathanbraun/yelpmap/MainActivity.java
@@ -2,39 +2,54 @@ package com.nathanbraun.yelpmap;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.nathanbraun.yelpmap.com.nathanbraun.yelpapi.YelpAPI;
 
 
 public class MainActivity extends ActionBarActivity {
+    private YelpAPI yelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //RelativeLayout layout = (RelativeLayout) this.findViewById(R.id.act_main_layout);
+        yelp = new YelpAPI();
     }
 
-    /*We are not using an OptionsMenu at this point in time.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem searchItem = menu.findItem(R.id.searchView);
+        SearchView search = (SearchView) searchItem.getActionView();
+        Toast.makeText(this, "here", Toast.LENGTH_SHORT).show();
+        search.setOnQueryTextListener(new YelpSearchListener());
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
         return super.onOptionsItemSelected(item);
     }
-    */
+
+    public class YelpSearchListener implements SearchView.OnQueryTextListener {
+        @Override
+        public boolean onQueryTextSubmit(String s) {
+            Toast.makeText(MainActivity.this, yelp.search("Mexican"), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String s) {
+            return false;
+        }
+    }
 }
